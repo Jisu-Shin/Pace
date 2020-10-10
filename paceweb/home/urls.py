c367733a0cec418fb7a6627ffe772a49833c8816
zsu <sjszzang3053@gmail.com>
2020-10-10 20:19:20 +0900

    django에 face_recog.py 넣음

--- paceweb/home/urls.py
+++ paceweb/home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from home import views
  
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
+    path('call_pop', views.call_chatbot, name='call_pop'),
 ]
