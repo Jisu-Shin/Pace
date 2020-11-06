7ee06609c324c37dae8dfd893bd42977c9ea46de
RyeonDeveloper <luvlik207@gmail.com>
2020-11-06 22:39:09 +0900

    병합 후 충돌 수정

--- paceweb/home/urls.py
+++ paceweb/home/urls.py
@@ -2,12 +2,7 @@ from django.urls import path
 from home import views
  
 urlpatterns = [
-<<<<<<< HEAD
     path('', views.index, name='index'),
     path('call_pop', views.call_pop, name='call_pop'),
     path('call_cam', views.call_cam, name='call_cam'),
-=======
-    path('', views.index),
-    path('history/', views.history,),
->>>>>>> ryeon
 ]
